f1922ae9b74fe72260b3caceea78690c3e611eea
CheesyDevil <seth.white@ucas-edu.net>
2025-08-20 10:46:09 -0600

    Made imput notes and Hello world

---
+++ Notes/Imputs.py
@@ -0,0 +1,10 @@
+#SW 2nd Inputs and outputs notes
+
+#imputs gives information to the program
+#allows variability
+# to get a number use num=input("give me a number.")
+#imput is the keyword next to the keyword. you need closed paranthisis, inside parenthesis you give the user instruction
+color=input("give me a color:")
+print("Your color is", color)
+
+#pick 1 method 
---
+++ practices/hello_world.py
@@ -0,0 +1,4 @@
+#SW 2nd Hello world
+
+name=input("State your name human:")
+print("Your name is", name,"... human")
