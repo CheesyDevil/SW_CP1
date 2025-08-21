52bcc144dd885f617066055b000555900c37529b
CheesyDevil <seth.white@ucas-edu.net>
2025-08-21 10:51:08 -0600

    Varables notes created

---
+++ Notes/variables.py
@@ -0,0 +1,27 @@
+# SW 2nd Variables Notes
+
+
+#What is a variable?
+   #A container that holds information
+#List the steps for writing a variable
+  #variablename=content
+#What is the difference between declaration and initialization of a variable?
+  # Declaration is setting the variable while initialization is putting initial value
+#How do you reset a variable?
+  # You set the variables value as something else
+#How do you print variables?
+  #print(variable)
+#Why are variables important in programming?
+  #so you can have versitile pieces
+#What are the 3 naming systems used in programming?
+#Why can't we have spaces when we name things in python?
+  #they were designed to only to use underlines
+#What things should you think about when coming up with a variable or function name?
+  #the actual function of the variable/function
+#Why must variable names be specific?
+  #so you know what's going on
+#Why should names be short?
+  #do you don't waste your time
+
+
+
