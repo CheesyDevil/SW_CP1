8184d5d3a388d893c85ab367ca00711b85c5051d
CheesyDevil <seth.white@ucas-edu.net>
2025-08-22 10:51:12 -0600

    Who are you

--- Notes/variables.py
+++ Notes/variables.py
@@ -2,7 +2,7 @@
 
 
 #What is a variable?
-   #A container that holds information
+  #A container that holds information
 #List the steps for writing a variable
   #variablename=content
 #What is the difference between declaration and initialization of a variable?
@@ -12,16 +12,14 @@
 #How do you print variables?
   #print(variable)
 #Why are variables important in programming?
-  #so you can have versitile pieces
+  #so you can have versatile pieces
 #What are the 3 naming systems used in programming?
+  #snake_case=no spaces, all lowercase, used to name files/variables. camelCase= capitalize all words except the first, no spaces of underscores, names functions. PascalCase+ capitalize all words, no spaces/underscores, names classes
 #Why can't we have spaces when we name things in python?
-  #they were designed to only to use underlines
+  #they were designed to go to the next thing after spaces
 #What things should you think about when coming up with a variable or function name?
   #the actual function of the variable/function
 #Why must variable names be specific?
   #so you know what's going on
 #Why should names be short?
   #do you don't waste your time
-
-
-
---
+++ projects/who_are_you.py
@@ -0,0 +1,14 @@
+#SW 2nd Who are you?
+
+dictionary={"yes", "yea", "correct", "indeed"}
+
+name=input("What doth be thou name my good fellow? ")
+age=input("And how many years have been on this world? ")
+color=input("And what pray tell is the colour most fair in your eyes? ")
+print("So to surmise thy name is," , name,"thy age is," , age, "and thy colour devine is" , color, "? ")
+if awnser == dictionary:
+    print("huzzah")
+
+else:
+    print("How dare thou desive me in this way sinner!")
+
