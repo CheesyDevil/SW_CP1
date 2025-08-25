80f31836f908451615cb7fdbfa3b35baab2fb1c5
CheesyDevil <seth.white@ucas-edu.net>
2025-08-25 10:28:00 -0600

    numbers starter and finished who are you

---
+++ Starters/numbers.py
@@ -0,0 +1,3 @@
+#SW 2nd Identifying numbers
+
+#step1:5 step2:10 step3:0 step4:32
--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -1,14 +1,13 @@
 #SW 2nd Who are you?
 
-dictionary={"yes", "yea", "correct", "indeed"}
-
 name=input("What doth be thou name my good fellow? ")
 age=input("And how many years have been on this world? ")
 color=input("And what pray tell is the colour most fair in your eyes? ")
 print("So to surmise thy name is," , name,"thy age is," , age, "and thy colour devine is" , color, "? ")
-if awnser == dictionary:
+awnser=input("Is that correct? ")
+if awnser == "yes":
     print("huzzah")
 
 else:
-    print("How dare thou desive me in this way sinner!")
-
+    print("How dare thou decive me in this way, sinner!")
+    
