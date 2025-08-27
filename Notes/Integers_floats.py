e1f3e80ba98b6910f4b15aec7ed9db864e6077cd
CheesyDevil <seth.white@ucas-edu.net>
2025-08-27 10:50:51 -0600

    Notes

---
+++ Notes/Integers_floats.py
@@ -0,0 +1,21 @@
+#SW 2nd Integers and Floats Notes
+
+
+#What are the 2 types of numbers and what do they hold?
+  #Integers=whole_numbers , Float=number with decimal point
+#How do I write an integer (Break apart the syntax)
+  #you type in the number without decimal point for integer and with for a float
+#How do we use arithmetic operations in python?
+  #Addition= + , Subtraction= - , exponents= ** , multiplication= * , division= / , Modulo= % (gives the remainder), Integer division= // (gives nearest whole number without the remainder)
+#How do we calculate exponents? 
+  # **
+#What is floor division?
+  
+#What is Mod (also called modulus and modulo)?
+  #Gives you the remainder of the division problem
+#How do we change data to an integer?
+  
+#How do we change data to a float?
+  
+#How do we round a number?
+  
