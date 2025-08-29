c29359ac1d7831d58f1a050098efe985f159db75
CheesyDevil <seth.white@ucas-edu.net>
2025-08-29 10:35:06 -0600

    made string notes

---
+++ Notes/strings.py
@@ -0,0 +1,15 @@
+#SW 2nd Strings notes
+
+
+#What is a string?
+Why are they called strings?
+How do I make a string?
+Why do we use strings?
+Does it matter if I use single or double quotes?
+How do we get the length of our string?
+What is an escape character?
+What does it mean to concatenate a string?
+How do you concatenate a string?
+How do I slice a string?
+What is index?
+How do we count indexes?
