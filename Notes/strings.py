be2fde918990699f447ae9ebbf8f93d1c0d240ac
CheesyDevil <seth.white@ucas-edu.net>
2025-09-02 10:51:05 -0600

    notes update

--- Notes/strings.py
+++ Notes/strings.py
@@ -2,14 +2,26 @@
 
 
 #What is a string?
-Why are they called strings?
-How do I make a string?
-Why do we use strings?
-Does it matter if I use single or double quotes?
-How do we get the length of our string?
-What is an escape character?
-What does it mean to concatenate a string?
-How do you concatenate a string?
-How do I slice a string?
-What is index?
-How do we count indexes?
+  #characters/symbols held together quotation marks
+#Why are they called strings?
+  # because the characters are strung together with the quotation marks
+#How do I make a string?
+  # "content of string"
+#Why do we use strings?
+  # so we can use words at all
+#Does it matter if I use single or double quotes?
+  # Not in python
+#How do we get the length of our string?
+  #len(what I want the lengeth of)
+#What is an escape character?
+  # \
+#What does it mean to concatenate a string?
+  #to put 2+ strings together into a single string
+#How do you concatenate a string?
+  # string 1 + string 2
+#How do I slice a string?
+  # string[starting index : ending index]
+#What is index?
+  # the location of a prticular character in a string 
+#How do we count indexes?
+  #
