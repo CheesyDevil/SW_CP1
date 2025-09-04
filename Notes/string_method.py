ec101ecfe4fb99f94ec0a591cdb9aaa05904ca2b
CheesyDevil <seth.white@ucas-edu.net>
2025-09-04 10:45:34 -0600

    string method notes

---
+++ Notes/string_method.py
@@ -0,0 +1,27 @@
+#SW 2nd String Method Notes
+
+#What is a method?
+  #prebuilt code ued for a specific data type
+#How do I write a method?
+  #go to string adn add .method_name()
+#What methods can I use to change the formatting of my string?
+  #format of just use f
+#What methods allow me to search the contents of my string?
+  # find
+#What methods allow me to change the contents of my string?
+  #replace
+#Why is it important to know what type of output a method will give me?
+  #so I can properly use them
+
+
+#methods:
+.strip() # gets rid of spaces on ends
+.lower() #lowercses
+.upper() #capitalizes everything
+.capitalize() #capitalizes the fist letter
+.title() #capitalizes irst letter of evrey word
+.isdigit() # checks if it contains a number
+.find() #finds a set of characters word and shows the begining index
+.slice() # removes parts of string
+.replace() # changes set of characters with another set of characters
+.format() #allows you to 
