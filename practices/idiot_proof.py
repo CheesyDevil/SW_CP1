1258f8b21029ceda4816e543fdb04ca51a9730c6
CheesyDevil <seth.white@ucas-edu.net>
2025-09-05 10:49:41 -0600

    Almost finished Idiot proof

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,4 +1,17 @@
 # SW 2nd Idiot proof
 
-name=input("what is your name? ").strip().lower().title
-name=
+name=input("What is your name? ").strip().lower().title()
+
+print(name)
+phone=input("What is your phone number? ").strip().replace("-" , "").replace(" " , "")
+phone_list=list(phone)
+phone_list.insert(3, " ")
+phone_list.insert(7, " ")
+phone= "".join(phone_list)
+print(phone)
+
+gpa=input("What is your GPA? ").strip()
+gpa=float(gpa)
+(gpa_r)=round(gpa, 2)
+gpa_s=str(gpa_r)
+print(gpa_s)
