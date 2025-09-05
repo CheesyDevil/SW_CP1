6a678de9aa4ba24ec387a5310ae37fa15bf7c99e
CheesyDevil <seth.white@ucas-edu.net>
2025-09-05 18:40:32 +0000

    finished Idiot Proof

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -15,3 +15,5 @@ gpa=float(gpa)
 (gpa_r)=round(gpa, 2)
 gpa_s=str(gpa_r)
 print(gpa_s)
+
+print("So your real name is " , name " your real phone number is " , phone , " and your GPA is about" , gpa_s ,".")
