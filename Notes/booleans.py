74d76ea9194443fe4e2ae9bbc56f5af7a8615180
CheesyDevil <seth.white@ucas-edu.net>
2025-09-18 10:42:04 -0600

    Made Boolean Notes

---
+++ Notes/booleans.py
@@ -0,0 +1,28 @@
+#What can a Boolean hold? (i.e. what is a Boolean?)
+  #True or Flase
+#What are Booleans normally used for?
+  #to check a variable
+#What does bool() let you do?
+  # check if something has a value
+#What values are considered True? 
+  # any information
+#What values are considered False?
+  #0 and ""
+#What are some python built-in functions that return a Boolean value?(Try and find 1 more than the example you are given!)
+  #bool() , isupper() , isalnum()
+#What are the simple data types?
+  #data types that only contain 1 peice of information (Strings,Floats,Integers,Booleans)
+#What would you use each data type for?
+  #integers are for whole numbers, floats are for numbers with decimals, string are for words, and booleans are for True/Flase
+
+
+import datetime as date
+
+name="seth"
+
+print(name.isalpha())
+
+
+#isupper() #checks if it's uppercase
+#isalpa() #checks if its only letters
+#isalnum() #checks if all numbers
