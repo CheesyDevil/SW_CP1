e5c380b276718622f7ea0d17e724fdfb497ec574
CheesyDevil <seth.white@ucas-edu.net>
2025-09-23 10:42:16 -0600

    Made Elif and Logical Notes

---
+++ Notes/Elif_and _logical_operators.py
@@ -0,0 +1,30 @@
+#SW 2nd Elif and Logical operators notes
+
+
+
+#How do we check more than one condition in python?
+  #by using elif
+#How many else if statements can you create? 
+  #as many as you want
+#Why does the order of your statements matter in a conditional?
+  #so that there aren't any values that slip through the cracks
+#How do we use boolean instead of boolean statements? 
+homework=False
+chores=True
+
+if homework and chores:
+    print("you can can go to your friends hosuse")
+elif not chores:
+    print("Go do your chores")
+else:
+    print("Do your homework")
+#What are the logical operators?
+  #and(CHECKS MULTIPLE CONDITIONS)/not(CHECKS IF CONDITION IS FALSE)/or(CHECKS IF ONE OF THE CONDITIONS IS TRUE)
+#How are logical operators used to change the outcome of a condition?
+  #NOT can reverse it, AND can make a condtion more complex, OR makes a condtion have more ways to activate
+#What does pass do?
+  #pass is for filling in outputs for conditions
+
+
+
+
