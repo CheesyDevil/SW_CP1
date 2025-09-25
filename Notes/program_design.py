76d92332bca8d0047a02bd93f172b978f5fa8e44
CheesyDevil <seth.white@ucas-edu.net>
2025-09-25 10:44:52 -0600

    Made Program Design Notes

---
+++ Notes/program_design.py
@@ -0,0 +1,29 @@
+#SW 2nd Program Design notes
+
+
+#What is the software development life cycle?
+  #It is the Cycle that is used to slowly but surely solve problems
+#What are the steps in the software development life cycle?
+  #1:requirements analisys (what needs to happen) 
+  # 2:Planning (Designating jobs) 
+  # 3:Write the code 
+  # 4:Testing(finding the errors and fix them)
+  # 5:Maintenance and release(more debugging!)
+#Why do we use the software development life cycle?
+  #It is found to be a very effective method for making and fixing your code
+#What are the 3 types of bugs that are most likely to occur in a program?
+  #Syntax
+  #Run-time
+  #Logic 
+#Where do each of these bugs come from?
+  #The code pieces don't interact as you expected 
+  #Issue that happens while the code is running/being compiled
+  #incorrect reasoning/design flaws
+#How do we resolve each type of bug?
+  #by finding where and why an issue happened and fixing it
+  #check the software/hardware you are running it on
+  #fixing the logic/reasoning
+#What is debugging?
+  #The act of fixing problems in code
+#Why do I need to be comfortable with debugging? 
+  #Because the majority of time spent coding is debugging
