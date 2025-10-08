035cef115b39c129b7e5204538290b22650652bd
CheesyDevil <seth.white@ucas-edu.net>
2025-10-08 10:50:59 -0600

    made psudocode

---
+++ Notes/psudocode.py
@@ -0,0 +1,13 @@
+#SW 2nd Psudocode Notes
+
+
+#What is pseudocode
+    #writting out steps in plain english
+#Why do we write pseudocode? 
+    #so you can break it down into small simple steps
+#How do you write pseudocode?
+    #By outlinig what you need to have happen in the logic
+#What kinds of words to we swap out for keywords in python? 
+    #Capitalize variables, print becomes display/show,for or while become loop unlkess it's a loop,elif becomes orif/otherwise, else stays
+#How can it be helpful to try multiple versions of your pseudocode?
+    #it gives you more insight on how to tackle the problem letting you come to a better conclution
