64c50d8dd28c2f2cf63cfce9f437f3e86a53c189
CheesyDevil <seth.white@ucas-edu.net>
2025-10-21 10:38:14 -0600

    made function notes

---
+++ Notes/Function.py
@@ -0,0 +1,36 @@
+#SW 2nd Function Notes
+
+
+
+#What is the purpose of a function?
+    #to accomplish a task more easily
+
+#How do you write functions?
+player_hp=1
+
+def attack(dmg):
+    return player_hp-dmg
+
+#Why should functions be abstract?
+    #So that they can apply to as many things as possible so they can simplify things as much as possible to make your code more modular
+
+#how do functions increase readability of a program?
+    # they allow people to see your thought process and make connections on what things do
+
+#what does modularity have to do with functions?
+    #Functions help make your code more modular by compacting it
+
+#How do arguments and parameters work together?
+    #paramaters is the stuff in the parenthesis when the function is defined and arguements are the inputs that are put in place of the parameters
+
+#What are return statements? What do they do?
+    #the return stements are the outputs of the function and make the function do things
+
+#What is the scope of a variable?
+    #the scope of the variable is where the variable can be used; if it's local its scope is the function it is assigned to;if it's global it can be accessed from anywhere. 
+
+#How are local and global variables different?
+    #local variables are only inside functions while global variables canbe accessed from anywhere
+
+#Why is it bad practice to make changes to global variables inside a function?
+    # because it can cause problems outside of the function
