4bcf4cc13c841e78dc0a2c1937f50818bfcf84f4
CheesyDevil <seth.white@ucas-edu.net>
2025-10-21 10:50:21 -0600

    created cesar cypher practice

---
+++ practices/cesar_cypher.py
@@ -0,0 +1,12 @@
+#SW 2nd Cesar Cypher encoder and decoder
+
+
+#reminder: use "ASCII" python string method
+ 
+print(f"a= {ord("a")}") #Finds index for character
+print(f"100= {chr(100)}") #Finds character for index
+
+#create input for user 
+
+
+#create function to cypher the input
