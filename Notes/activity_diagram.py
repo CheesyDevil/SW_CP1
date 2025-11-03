9f94ce48830518a0e8a28bc5148001966f09f4dc
CheesyDevil <seth.white@ucas-edu.net>
2025-11-03 10:31:09 -0700

    Made activity diagram notes

---
+++ Notes/activity_diagram.py
@@ -0,0 +1,27 @@
+#SW 2nd Activity Diagram Notes
+
+
+#What does UML stand for?
+    #Unified Modeling Language
+#When would you use an Activity Diagram?
+    #whenever there needs to be an order of actions and results for those actions
+#What symbol indicates the start of an activity diagram?
+    #A solid black circle
+#What symbol indicates an action in the activity diagram?
+    #a round cornered rectangle
+#What does a decision node do?
+    #determines which action is taken resulting in differing results
+#Does the direction of the arrows matter?
+    # yes it shows which action causes what
+#What is a fork?
+    # A place where you have multiple choices for an action that can have different results 
+#What is a join?
+    #a place wher previously seperate couses of acttion come together to reach the same result
+#How is a join different from a merge?
+    # a merge is of the same structure while a join combines multiple structures
+#What is a swimlane?
+    # a visual tool that is used to show which ations belong to what group
+#What do swimlanes do?
+    #help the programmer visualize what goes where and what methods need to happen in a specific action
+#How do activity diagrams end?
+    # a black circle in a outlined circle
