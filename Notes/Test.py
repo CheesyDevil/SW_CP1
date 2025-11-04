c4636b4dee2e833edfd56937e8181076a88560b5
CheesyDevil <seth.white@ucas-edu.net>
2025-11-04 10:49:14 -0700

    finished Csesar Cypher

--- Notes/Test.py
+++ Notes/Test.py
@@ -1,23 +1,24 @@
-
-mess=input("character: ")
-for char in mess:
-    if char.isupper():
-        if ord(char)<90:
-            e=ord(char) 
-            e+=1
-            char.replace(chr(e))
+def e:
+    mess=input("character: ")
+    mess_out=''
+    for char in mess:
+        if char.isupper():
+            if ord(char)<90:
+                e=ord(char) 
+                e+=1
+                mess_out+=(chr(e))
+            else:
+                e=65
+                mess_out+=(chr(e))
+        elif char.islower():
+            if ord(char)<122:
+                e=ord(char) 
+                e+=1
+                mess_out+=(chr(e))
+            else:
+                e=97
+                mess_out+=(chr(e))
         else:
-            e=65
-            char.replace(chr(e))
-    elif char.islower():
-        if ord(char)<122:
-            e=ord(char) 
-            e+=1
-            char.replace(chr(e))
-        else:
-            e=97
-            char.replace(chr(e))
-    else:
-        ord(char)==ord(char)
-    print(char)
-print(mess)
+            ord(char)==ord(char)
+        print(char)
+    print(mess_out)
--- practices/cesar_cypher.py
+++ practices/cesar_cypher.py
@@ -4,71 +4,115 @@
 #reminder: use "ASCII" python string method
 def encode(): #create encoding function
     mess=input("What is the message you would like to have cyphered\n ") #create input for what is being cyphered 
-    cyph_chan=input("How many timeswyould you like to chyper this message?\n")#create input for cypher number
+    cyph_chan=int(input("How many times would you like to chyper this message?\n"))#create input for cypher number
+    out=''#output
 
-    if cyph_chan>0:
+    while cyph_chan>0:
         for char in mess:
             if char.isupper():
                 if ord(char)<90:
                     e=ord(char) 
                     e+=1
-                    char=chr(e)
+                    out+=(chr(e))
                 else:
                     e=65
-                    char=chr(e)
+                    out+=(chr(e))
             elif char.islower():
                 if ord(char)<122:
                     e=ord(char) 
                     e+=1
-                    char=chr(e)
+                    out+=(chr(e))
                 else:
                     e=97
-                    char=chr(e)
+                    out+=(chr(e))
             else:
-                ord(char)=ord(char)
-            cyph_chan-=cyph_chan
-    elif cyph_chan<0:
+                out+=char
+        cyph_chan-=1
+
+    while cyph_chan<0:
+        for char in mess:
+            if char.isupper():
+                if ord(char)<90:
+                    e=ord(char) 
+                    e-=1
+                    out+=(chr(e))
+                else:
+                    e=65
+                    out+=(chr(e))
+            elif char.islower():
+                if ord(char)<122:
+                    e=ord(char) 
+                    e-=1
+                    out+=(chr(e))
+                else:
+                    e=97
+                    out+=(chr(e))
+            else:
+                out+=char
+        cyph_chan+=1
+    
+    print(f" your final message is:\n {out}")
+
+
+def decode(): #create encoding function
+    mess=input("What is the message you would like to have decoded\n ") #create input for what is being cyphered 
+    cyph_chan=-int(input("How many times was this message cyphered?\n"))#create input for cypher number
+    out=''#output
+
+    while cyph_chan>0:
+        for char in mess:
+            if char.isupper():
+                if ord(char)<90:
+                    e=ord(char) 
+                    e+=1
+                    out+=(chr(e))
+                else:
+                    e=65# if the letter is Z it loops back to A
+                    out+=(chr(e))
+            elif char.islower():
+                if ord(char)<122:
+                    e=ord(char) 
+                    e+=1
+                    out+=(chr(e))
+                else:
+                    e=97# if the letter is z it loops back to a
+                    out+=(chr(e))
+            else:
+                out+=char
+        cyph_chan-=1
+
+    while cyph_chan<0:
         for char in mess:
             if char.isupper():
                 if ord(char)>65:
                     e=ord(char) 
                     e-=1
-                    char=chr(e)
+                    out+=(chr(e))
                 else:
-                    e=90
-                    char=chr(e)
+                    e=90# if the letter is A it loops back to Z
+                    out+=(chr(e))
             elif char.islower():
                 if ord(char)>97:
                     e=ord(char) 
                     e-=1
-                    char=chr(e)
+                    out+=(chr(e))
                 else:
-                    e=122
-                    char=chr(e)
+                    e=122# if the letter is a it loops back to z
+                    out+=(chr(e))
             else:
-                ord(char)=ord(char)
-            cyph_chan+=cyph_chan
-    else:
-        mess+mess
-
-def decode(): #create encoding function
-    mess=input("What is the message you would like to have decoded\n ") #create input for what is being cyphered 
-    cyph_chan=input("How many times was this message chypered\n")#create input for cypher number
+                out+=char
+        cyph_chan+=1
+    
+    print(f" your final message is:\n {out}")
 
 
 awn=input("Would you like to encode or decode?\n enter '1' for encode and '2' for decode\n") #add input for wether they are doing encoding or decoding
 if awn=="1":#add function for directing whether they go to the encode or decode function
-    encode
+    encode()
 elif awn=="2":
-    decode
+    decode()
 else:
-    print("please entter valid input")
-
-
-print(f"A= {ord("Z")}") #Finds index for character
-print(f"100= {chr(100)}") #Finds character for index
-
-
+    print("please enter valid input")
 
 
 #create function to cypher the input
