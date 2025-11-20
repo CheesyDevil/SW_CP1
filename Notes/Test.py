8cd6814049d0d8f7a082977fead8edc8fc9f13f0
CheesyDevil <seth.white@ucas-edu.net>
2025-11-20 10:43:26 -0700

    finished flexy calculator

--- Notes/Test.py
+++ Notes/Test.py
@@ -1,27 +1,14 @@
-def e:
-    mess=input("character: ")
-    mess_out=''
-    for char in mess:
-        if char.isupper():
-            if ord(char)<90:
-                e=ord(char) 
-                e+=1
-                mess_out+=(chr(e))
-            else:
-                e=65
-                mess_out+=(chr(e))
-        elif char.islower():
-            if ord(char)<122:
-                e=ord(char) 
-                e+=1
-                mess_out+=(chr(e))
-            else:
-                e=97
-                mess_out+=(chr(e))
-        else:
-            ord(char)==ord(char)
-        print(char)
-    print(mess_out)
+def print_list_contents(*args):
+    """
+    Prints each item passed as an argument.
+    """
+    print("Printing list contents:")
+    for item in args:
+        print(item)
+
+# Example usage:
+my_list = [1, 2, 3, "hello", True]
+print_list_contents(*my_list)
 
 
 #git config --global user.name "github username"
--- practices/fexy_calculator.py
+++ practices/fexy_calculator.py
@@ -1,6 +1,12 @@
 #SW 2nd Flexible Calculator
 
 
+def outlist(*args):
+    for item in args:
+        print(item)
+
+
+
 def main():#Main loop
     num_list=[]#list holding all of the inputted numbers
     output=0#final output
@@ -16,29 +22,45 @@ def main():#Main loop
         else:
             print("Please use a valid input")
     num_list.sort()#organizes numbers to be easier to use
-    print(f"Available opperations:\nsum (adds all numbers together),\n mean (finds the average of the numbers),\nmin (finds the lowest number),\nmax (finds highest number),\nproduct (multiplies all numbers together)")
+    print(f"Available opperations:\nsum (adds all numbers together),\nmean (finds the average of the numbers),\nmin (finds the lowest number),\nmax (finds highest number),\nproduct (multiplies all numbers together)")
     opperation=input("which opperation do you want to use?\n").strip().lower()#imput for which opperation is used
 
-    if opperation=="sum": #adds all numbers together
-        for numb in num_list:
-            output+=numb
-    elif opperation=="mean":
-        for numb in num_list:
-            output+=numb
+    while output==0:#loop to make sure operation happens
+        if opperation=="sum": #adds all numbers together
+            for numb in num_list:
+                output+=numb
+        elif opperation=="mean":
+            for numb in num_list:
+                output+=numb
             output=output/len(num_list)
-    elif opperation=="min":
-        output=num_list[0]
-    elif opperation=="max":
-        output=num_list[len(num_list)]
-    elif opperation=="product":
-        output=1#set to 1 so multplying works
-        for numb in num_list:
-            output=1
-            output=output*numb
+        elif opperation=="min":
+            output=num_list[0]#using first number in list which is the smallest because of 
+        elif opperation=="max":
+            output=num_list[len(num_list)-1]#finding the last index on list to output
+        elif opperation=="product":
+            output=1#set to 1 so multplying works
+            for numb in num_list:
+                output=output*numb
+        else:
+            print("please enter a valid ")
+
+    print(f"calculating the {opperation} of:")
+    print(*num_list, sep= ", ")
+    print(f"Result: {output}")
 
 
 
 
-def calc():#funtion asking if person is doing another calculation
+print("Welcome to flexible calculator")
+main()
+awn=True
+while awn:
     awn=input("would you like to preform another calculation? (Yes/No)").strip().lower().title()
+    if awn=="Yes":
+        main()
+    if awn=="No":
+        awn=False#ends loop and calculator sesion
+    else:
+        print("Please enter a valid input")
+
 
