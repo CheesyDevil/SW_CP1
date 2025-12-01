f8e2d256da85d2f915940655dff47cc7717f3a9f
CheesyDevil <seth.white@ucas-edu.net>
2025-12-01 10:49:16 -0700

    Started Final project psuedocode

---
+++ projects/Rougelike_final.py
@@ -0,0 +1,21 @@
+#SW 2nd Cat Rougelike Final Project
+
+
+#inport random
+
+#create 7 dictionaries for indexes, (common cards, uncommon cards, rare cards, epic cards, legendary cards, basic cats, elite cats) with a number as key and a list as the value with the list holding the different stats as well as the description
+"""
+cat_example :{ 1("index number"),['orange cat'("base description"),"Food and throwing toys"("weaknesses"),'boxes'("resistances"),3 ("attack bonus"), 20 ("base friendliness")]}
+card_example:{1('index number'),["melee toy effectiveness"("card name"), "melee"("affected toy"),'+' ("bonus type"), 2 ("bonus number")]}"""
+
+#create player dictionary with (player hp, bandages, cards list, food modifiers [flat, mult], throwable modifiers [flat, mult], melee modifiers [flat, mult], box modifiers [flat, mult], overall mult, luck)
+
+
+
+#create a friendliness variable
+
+
+
+#make funtion to randomly pick cat from dictionary, print("A (base description) is approachinig you."), and set friendliness to ("Base friendliness") 
+
+#make function fo rchecking cat that displays ("this cat is weak to ("weaknesses"), resistant to ("resistances"), and has (friendliness) left") 
